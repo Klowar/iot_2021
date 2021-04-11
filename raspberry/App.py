ec451a50d5c26efb5e8bf95cf5e0eadc462c9807
Aider <ideeer4@gmail.com>
2021-04-11 20:03:56 +0300

    Listen for settings

--- raspberry/App.py
+++ raspberry/App.py
@@ -1,3 +1,5 @@
+import psutil
+import alsaaudio
 import multiprocessing
 from playsound import playsound
 from NetworkListener import MqttListener
@@ -7,6 +9,11 @@ HOST = "broker.hivemq.com"
 TOPIC = "radio/raspberry_itis_orion"
 SETTINGS = "radio/raspberry_itis_orion/settings"
 
+PLAYBACKS = {
+    STOP: "stop",
+    PLAY: "play"
+}
+
 def daemon(path, block):
     p = multiprocessing.current_process()
     playsound(path, block)
@@ -14,19 +21,34 @@ def daemon(path, block):
 class App:
 
     def __init__(self):
+        self.mixer = alsaaudio.Mixer()
         self.name = "app"
-        self.processThread = None
+        self.musicProcess = None
         self.listener = MqttListener(HOST, TOPIC)
+        self.settingListener = MqttListener(HOST, SETTINGS)
 
     def listen(self):
         self.listener.subscribe(self.play)
+        self.settingListener.subscribe(self.applySettings)
         self.listener.start()
         # Todo listen rfid, mqtt next
 
+    def applySettings(self, json):
+        if (json.volume != None):
+            self.mixer.setvolume(json.volume)
+        if (json.playback == PLAYBACKS.STOP and self.musicProcess != None):
+            process = psutil.Process(self.musicProcess.pid)
+            process.suspend()
+        if (json.playback == PLAYBACKS.PLAY and self.musicProcess != None):
+            process = psutil.Process(self.musicProcess.pid)
+            process.resume()
+
+
+
     def play(self, json):
         print("Callback {}".format(json))
-        if (self.processThread != None and self.processThread.is_alive()):
-            self.processThread.kill()
+        if (self.musicProcess != None and self.musicProcess.is_alive()):
+            self.musicProcess.kill()
         path = Downloader.download(json.music)
-        self.processThread = multiprocessing.Process(target=daemon, args=(path, True))
-        self.processThread.start()
+        self.musicProcess = multiprocessing.Process(target=daemon, args=(path, True))
+        self.musicProcess.start()
