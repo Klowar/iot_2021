09e44e9000eff7233665e94a524486a0882f82e7
Aider <ideeer4@gmail.com>
2021-04-11 14:22:37 +0300

    Init

---
+++ raspberry/App.py
@@ -0,0 +1,10 @@
+class App:
+
+    def __init__(self):
+        self.name = "app"
+
+    def listen(self):
+        # Todo listen rfid, mqtt next
+        print("Listening")
+
+
---
+++ raspberry/index.py
@@ -0,0 +1,5 @@
+from App import App
+
+if __name__ == 'main':
+    app = App()
+    app.listen()
