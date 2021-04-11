a2700acdf33a0471c573957561c07ba32da6c661
Aider <ideeer4@gmail.com>
2021-04-11 17:43:58 +0300

    Use user agent

--- raspberry/Downloader.py
+++ raspberry/Downloader.py
@@ -1,5 +1,6 @@
 import hashlib
-import urllib.request
+from urllib.request import Request, urlopen
+
 
 
 class Downloader:
@@ -7,6 +8,10 @@ class Downloader:
     def download(url):
         filename = hashlib.md5(b'Hello World').hexdigest()
         path = '/tmp/{}'.format(filename)
-        urllib.request.urlretrieve(url, '/tmp/{}'.format(filename))
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        with open(path, 'wb') as newfile:
+            newfile.write(webpage)
+
         print("Downloaded {}, to {}".format(url, path))
         return path
