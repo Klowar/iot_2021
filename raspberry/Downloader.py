75735ee0b709eb79de343303d06c2a91afd56951
Aider <ideeer4@gmail.com>
2021-04-13 23:30:38 +0300

    Encoded urls

--- raspberry/Downloader.py
+++ raspberry/Downloader.py
@@ -6,7 +6,7 @@ import urllib
 class Downloader:
 
     def download(url):
-        filename = hashlib.md5(url).hexdigest()
+        filename = hashlib.md5(url.encode('utf-8')).hexdigest()
         path = '/tmp/{}.mp3'.format(filename)
         urllib.request.urlretrieve(url, path)
         print("Downloaded {}, to {}".format(url, path))
